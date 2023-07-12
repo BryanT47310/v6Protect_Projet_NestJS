9e314afc6ceb952ff4f688f973c90f9852b238a7
BryanT47310 <bryanparra47310@gmail.com>
2023-07-13 01:36:37 +0200

    commentaires

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -6,21 +6,21 @@ import { ProductsService } from './products.service';
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
-  //Récupération de tous les produits
+  //Récupération de tous les produits puis affichage dans la view products.hbs
   @Get()
   getAllProducts(@Res() res: Response) {
     const products = this.productsService.getAllProducts();
     res.render('products', { products });
   }
 
-  //Ajout d'un produit
+  //Ajout d'un produit puis redirection vers la view products.hbs
   @Post()
   addProduct(@Body() product: any, @Res() res: Response) {
     this.productsService.addProduct(product);
     res.redirect('/products');
   }
 
-  //Suppression du produit
+  //Suppression du produit puis redirection vers la view products.hbs
   @Post(':id')
   deleteProduct(@Param('id') id: string, @Res() res: Response) {
     this.productsService.deleteProduct(Number(id));
